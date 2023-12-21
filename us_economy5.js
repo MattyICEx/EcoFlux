cf2ab2a78caf45825d23949a679962f7076f93cc
Mateusz Pasynkiewicz <mattpasyn@gmail.com>
2023-12-20 17:33:36 -0800

    added fed funds rate - test 1

--- us_economy5.js
+++ us_economy5.js
@@ -2,6 +2,7 @@ window.onload = async () => {
     Wized.data.listen('v.Core_Inflation_Observations', async () => {
         let observationsInflation = await Wized.data.get('v.Core_Inflation_Observations');
         let observationsUnemployment = await Wized.data.get('v.unemployment_rate_observations');
+        let observationsFedFundsRate = await Wized.data.get('v.federal_funds_rate_observations');
         let dates = await Wized.data.get('v.dates');
 
         // Truncate leading zeros
@@ -9,6 +10,7 @@ window.onload = async () => {
         if (y === -1) y = observationsInflation.length; // If all are zeros, set y to length of array
         observationsInflation = observationsInflation.slice(y); // Remove leading zeros from observations
         observationsUnemployment = observationsUnemployment.slice(y); // Remove leading zeros from observations
+        observationsFedFundsRate = observationsUnemployment.slice(y); // Remove leading zeros from observations
         dates = dates.slice(y); // Remove corresponding dates
 
         // Parse the dates from "YYYY-MM-DD" format and convert them to "MM/YYYY" format using Moment.js
@@ -25,8 +27,8 @@ window.onload = async () => {
                     data: observationsInflation,
                     pointRadius: 0,
                     borderWidth: 1,
-                    borderColor: 'blue', // Set the line color to red
-                    backgroundColor: 'blue', // Set the point color to red (if you're using points)
+                    borderColor: 'red', // Set the line color to red
+                    backgroundColor: 'red', // Set the point color to red (if you're using points)
                 }]
             },
             options: {
@@ -133,8 +135,117 @@ window.onload = async () => {
             data: {
                 labels: dates,
                 datasets: [{
-                    label: 'Core Inflation',
+                    label: 'Unemployment Rate',
                     data: observationsUnemployment,
+                    pointRadius: 0,
+                    borderWidth: 1,
+                    borderColor: 'blue', // Set the line color to red
+                    backgroundColor: 'blue', // Set the point color to red (if you're using points)
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Unemployment Rate',
+                        color: 'White',
+                        font: {
+                            size: 24, // Set title font size (e.g., 24 for a larger title)
+                        }
+                    },
+                    legend: {
+                        display: false
+                    },
+                    zoom: {
+                        pan: {
+                            enabled: true,
+                            mode: 'x', // Enable panning only in the X direction
+                        },
+                        zoom: {
+                            wheel: {
+                                enabled: true,
+                            },
+                            pinch: {
+                                enabled: true,
+                            },
+                            mode: 'x', // Enable zooming only in the X direction
+                            limits: {
+                                x: { min: 'original', max: 'original' }, // Set zoom limits if needed
+                            },
+                        }
+                    },
+                    tooltip: {
+                        enabled: true,
+                        mode: 'index',
+                        intersect: false,
+                        callbacks: {
+                            label: function (context) {
+                                let label = context.dataset.label || '';
+                                if (label) {
+                                    label += ': ';
+                                }
+                                if (context.parsed.y !== null) {
+                                    label += context.parsed.y;
+                                }
+                                return label;
+                            }
+                        }
+                    }
+                },
+                hover: {
+                    mode: 'index',
+                    intersect: false
+                },
+                scales: {
+                    x: {
+                        ticks: {
+                            color: "#93afc5",
+                            autoSkip: false, // Prevent automatic label skipping
+                            maxRotation: 0, // Prevent label rotation
+                            minRotation: 0,
+                            padding: 10,
+                            callback: function (value, index, values) {
+                                var x = 5; // Replace with your desired number of parts
+                                var interval = Math.ceil(values.length / x); // Calculate the interval for labels
+
+                                // Always return the first and last label,
+                                // and optionally return the label if it's not too crowded
+                                if (index === 0 || index === values.length - 1 || index % interval === 0) {
+                                    return dates[value];
+                                } else {
+                                    // Implement logic here to determine if intermediate labels should be shown
+                                    // For example, return every nth label to reduce crowding
+                                    // Uncomment and set 'n' to the desired skip interval
+                                    // return index % n === 0 ? value : null;
+                                }
+                            }
+                        },
+                        grid: {
+                            color: "rgba(92, 118, 223, 0.2)" // Sets the color of the x-axis grid lines
+                        }
+                    },
+                    y: {
+                        ticks: {
+                            color: "#93afc5"
+                        },
+                        grid: {
+                            color: "rgba(92, 118, 223, 0.2)" // Sets the color of the x-axis grid lines
+                        }
+                    }
+                }
+            }
+        });
+        // Creating a professional and clean line chart with a dark blue background
+        const fedFundsRateCtx = document.getElementById('unemploymentChart');
+        const fedFundsRateChart = new Chart(fedFundsRateCtx, {
+            type: 'line',
+            data: {
+                labels: dates,
+                datasets: [{
+                    label: 'Federal Funds Rate',
+                    data: observationsFedFundsRate,
                     pointRadius: 0,
                     borderWidth: 1,
                     borderColor: 'green', // Set the line color to red
@@ -147,7 +258,7 @@ window.onload = async () => {
                 plugins: {
                     title: {
                         display: true,
-                        text: 'Core Inflation',
+                        text: 'Federal Funds Rate',
                         color: 'White',
                         font: {
                             size: 24, // Set title font size (e.g., 24 for a larger title)
